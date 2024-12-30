fd8eb7ab5f11bf7bee2f03703fa179be9b482735
sabil <billhikam2003@gmail.com>
2024-12-31 06:22:42 +0700

    prepare database connection

---
+++ src/config/database.js
@@ -0,0 +1,12 @@
+import { Sequelize } from "sequelize";
+
+const db = new Sequelize({
+    dialect: "mysql",
+    host: "localhost",
+    username: "root",
+    password: "admin24434",
+    database: "siakad",
+    port: 3306,
+});
+
+export default db
